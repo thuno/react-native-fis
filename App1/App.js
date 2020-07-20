918afd9263a856c74c52381376649fc445c2d386
thuno <61137042+thuno@users.noreply.github.com>
2020-07-20 11:04:16 +0700

    Create App.js

---
+++ App1/App.js
@@ -0,0 +1,19 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {View} from 'react-native';
+
+const App: () => React$Node = () => {
+  return (
+    <View> 
+    </View>
+  );
+};
+
+export default App;
